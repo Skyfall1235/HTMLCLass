c6d816c415e92cd64a587900d98577f0a88ece86
Wyatt Murray <113722636+Skyfall1235@users.noreply.github.com>
2024-09-30 19:26:34 -0400

    week6

---
+++ Week6/script.js
@@ -0,0 +1,5 @@
+const heading = document.getElementById('Heading');
+const BasicText = document.getElementById('BasicText');
+const ColorButton = document.getElementById('ColorButton');
+const NumberButton = document.getElementById('NumberButton');
+
