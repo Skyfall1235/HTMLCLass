6dc3e850b92c25ee2212a46c01eee0138bb9271d
Wyatt Murray <113722636+Skyfall1235@users.noreply.github.com>
2024-10-14 19:26:28 -0400

    beginning of hell (aka canvas drawings for js)

---
+++ week7/Canvas.js
@@ -0,0 +1,16 @@
+var convas = document.getElementById("game");
+var ctx = document.GetContext("2d");
+
+ctx.fillStyle = "#FF0000"
+ctx.fillRect(10, 10, 30, 50);
+
+//drawing lines
+ctx.moveTo();
+ctx.lineTo();
+ctx.clearRect();
+ctx.arcTo();
+ctx.stroke();
+ctx.fill();
+//colors
+ctx.fillStyle();
+ctx.strokeStyle();
