700d1d2c920a63c3bd539121272f85016bcf49dc
Wyatt Murray <113722636+Skyfall1235@users.noreply.github.com>
2024-12-02 17:29:25 -0500

    init

---
+++ Week14/game.js
@@ -0,0 +1,48 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 }
+        },
+        debug: false
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload () {
+
+}
+
+function create () {
+    
+}
+
+function update () {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+
+    if (gameOver) {
+        return;
+    }
+}
