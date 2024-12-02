1d7661efd574d4cbe458f35f38be85fdfd7d790d
Wyatt Murray <113722636+Skyfall1235@users.noreply.github.com>
2024-12-02 18:21:29 -0500

    Update game.js

--- Week14/game.js
+++ Week14/game.js
@@ -30,3 +30,15 @@ function update () {
 
 }
 
+
+//2 scenes, a main menu and a game.
+//game has 3 things that cycle enddlessly. easy stage, normal stage, bossstage
+//easy stage spawns a random amount of enemies
+//normal stage spawns more andlasts longer, but gives a power up
+//boss stage is bullet hell :)
+
+//requires a high score, and a live system. every time you kill the boss you get 1 extra life
+
+//power ups can be double bullets or shield for 15 sec
+
+//background is balck with scrolling itesm behind the player and enemies
